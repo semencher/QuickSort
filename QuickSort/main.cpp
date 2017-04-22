6e2549bd18d6e58e1357d4d951461196317afbf0
semencher <semen.cher@mail.ru>
2017-04-22 12:04:57 +0700

    Наполовину переделал в шаблон

--- QuickSort/main.cpp
+++ QuickSort/main.cpp
@@ -2,8 +2,13 @@
 
 int main()
 {
-	const size_t size = 10;
+	const size_t size = 10000000;
 	std::vector<int> vector = genRandomArray(size);
+	quickSort(vector.begin(), vector.end());
+	std::cout << "Size: " << size << ".\n";
+	std::cout << (std::is_sorted(vector.begin(), vector.end()) ? "Ok!" : "No!") << "\n";
 
+	int a = 0;
+	std::cin >> a;
 	return 0;
 }
--- QuickSort/quicksort.h
+++ QuickSort/quicksort.h
@@ -8,3 +8,7 @@
 std::vector<int> genRandomArray(size_t size);
 
 void showVectorInt(const std::vector<int> vector);
+
+void quickSort(std::vector<int>::iterator l, std::vector<int>::iterator r);
+
+void directInsertion(std::vector<int>::iterator l, std::vector<int>::iterator r);
